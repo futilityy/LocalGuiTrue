f63b8cf322a9930303415198cb7b64a9a9486079
futilityy <110702066+futilityy@users.noreply.github.com>
2022-09-26 19:52:55 +0100

    s
    s

---
+++ README.lua
@@ -0,0 +1,90 @@
+-- Made BY D00mshack (futility#9609)
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+
+local X = Material.Load({
+    Title = "D00000M Hub - Da hood",
+    Style = 4,
+    SizeX = 350,
+    SizeY = 350,
+    Theme = "Aqua",
+    ColorOverrides = {
+        MainFrame = Color3.fromRGB(255,255,255)
+    }
+})
+
+local Tab1 = X.New({
+    Title = "Popular"
+})
+
+local gamescriptbutton1 = Tab1.Button({
+    Text = "Exclusion Lock (OP)",
+    Callback = function()
+       loadstring(game:HttpGet("https://raw.githubusercontent.com/futilityy/ssssssssssssssssssssssssssssssssssssssssssssssss1222222222222222222121/main/README.lua"))()
+    end,
+    })
+
+    local gamescriptbutton2 = Tab1.Button({
+        Text = "Zell Hub",
+        Callback = function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackerProLuaStuff/zellhubnew/main/SCRIPTPRO.lua"))()
+        end,
+        })
+
+        
+
+local Tab2 = X.New({
+    Title = "Extra"
+})
+
+local Extrascriptbutton1 = Tab2.Button({
+    Text = "Oblivity Desync",
+    Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/ADWcVktH", true))()
+
+    end,
+    })
+
+    local Extrascriptbutton2 = Tab2.Button({
+        Text = "Krypt Hub(Skidware)",
+        Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/futilityy/Krpttt/main/README.lua", true))()
+   
+        end,
+        })
+        local Extrascriptbutton3 = Tab2.Button({
+            Text = "Infinite Yield",
+            Callback = function()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source", true))()
+            end,
+        })
+
+local Tab3 = X.New({
+    Title = "Privates"
+})
+
+        local Privatesscriptbutton1 = Tab3.Button({
+            Text = "Reticle Private",
+            Callback = function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/futilityy/rszxc1/main/README.lua", true))()
+
+            end,
+        })
+
+
+
+        local Privatesscriptbutton2 = Tab3.Button({
+            Text = "Expunged Lock V2",
+            Callback = function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/futilityy/f/main/README.lua", true))()
+
+            end,
+        })
+
+
+
+
+
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# LocalGuiTrue
-s
